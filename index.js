0c8a74f7e1d79fd99681ef0524854bdd84d214b2
Pawan Kumar <pyadav.workmail@gmail.com>
2024-11-16 20:45:07 +0530

    fixed invoice image cors issue

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ require('dotenv').config()
 // app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 app.use(cors({
   origin: '*',  // Replace with your frontend's URL
 }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Configure CORS options
 // const corsOptions = {
